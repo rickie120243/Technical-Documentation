b4e38e3c14f1d6432de07039cce1d4c89f8c55c1
rickie120243 <rickie120243@gmail.com>
2015-09-16 17:05:12 +0800

    change main.html:frameset to div

--- ui.js
+++ ui.js
@@ -1,11 +1,16 @@
-//var showterm=function(term){
-//	return "<b>"+term.entry+"("+term.page+")"+"</b> : "+"藏文解釋 : "+term.tdefinition+" 中文解釋 : "+term.cdefinition;
-//}
+var showterm=function(term){
+	return "<b>"+term.entry+"("+term.page+")"+"</b> : "+"藏文解釋 : "+term.tdefinition+" 中文解釋 : "+term.cdefinition;
+}
 var showentry=function(term){
-	return "<div onClick='showdetails()'>"+term.entry+"("+term.page+")</div>";
+	return "<div onClick='showdetails(this.innerHTML)'>"+term.entry+"("+term.page+")</div>";
 }
 
 var doentrySearch=function(tofind){
+	var arr=entrySearch(tofind);
+	document.getElementById("display2").innerHTML=arr.map(showentry).join("<br/>");
+}
+
+var showdetails=function(tofind){
 	var arr=entrySearch(tofind);
 	document.getElementById("display1").innerHTML=arr.map(showentry).join("<br/>");
 }
