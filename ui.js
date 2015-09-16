061f7945a890169d3d8a3820b3894fb8110a4447
rickie120243 <rickie120243@gmail.com>
2015-09-16 17:55:23 +0800

    fix showdetails

--- ui.js
+++ ui.js
@@ -1,5 +1,5 @@
 var showterm=function(term){
-	return "<b>"+term.entry+"("+term.page+")"+"</b> : "+"藏文解釋 : "+term.tdefinition+" 中文解釋 : "+term.cdefinition;
+	return "<b>"+term.entry+"("+term.page+")"+"</b> : <br/>"+"藏文解釋 : "+term.tdefinition+"<br/> 中文解釋 : "+term.cdefinition;
 }
 var showentry=function(term){
 	return "<div onClick='showdetails(this.innerHTML)'>"+term.entry+"("+term.page+")</div>";
@@ -7,10 +7,10 @@ var showentry=function(term){
 
 var doentrySearch=function(tofind){
 	var arr=entrySearch(tofind);
-	document.getElementById("display2").innerHTML=arr.map(showentry).join("<br/>");
+	document.getElementById("display1").innerHTML=arr.map(showentry).join("<br/>");
 }
 
 var showdetails=function(tofind){
-	var arr=entrySearch(tofind);
-	document.getElementById("display1").innerHTML=arr.map(showentry).join("<br/>");
+	var arr=entrySearch(tofind.replace(/\(\d+.\d+\)/,""));
+	document.getElementById("display2").innerHTML=arr.map(showterm).join("<br/>");
 }
