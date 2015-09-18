e4c4a30ac3789adcbe237f8c0cb83cf3f140c194
rickie120243 <rickie120243@gmail.com>
2015-09-18 10:14:13 +0800

    fix ui.js tab

--- ui.js
+++ ui.js
@@ -1,25 +1,24 @@
+var showterm=function(term){
+	return "<b>"+term.entry+"("+term.page+")"+"</b> : <br/>"+"藏文解釋 : "+term.tdefinition+"<br/> 中文解釋 : "+term.cdefinition;
+}
 
-	var showterm=function(term){
-			return "<b>"+term.entry+"("+term.page+")"+"</b> : <br/>"+"藏文解釋 : "+term.tdefinition+"<br/> 中文解釋 : "+term.cdefinition;
-		}
+var showentry=function(term){
+	var str = JSON.stringify(term,"","");
+	return "<div onClick='showdetails("+str+")'>"+term.entry+"("+term.page+")</div>";
+}
 
-	var showentry=function(term){
-			var str = JSON.stringify(term,"","");
-			return "<div onClick='showdetails("+str+")'>"+term.entry+"("+term.page+")</div>";
-		}
+var doentrySearch=function(tofind){
+	var arr=entrySearch(tofind);
+	document.getElementById("display1").innerHTML=arr.map(showentry).join("<br/>");
+}
 
-	var doentrySearch=function(tofind){
-			var arr=entrySearch(tofind);
-			document.getElementById("display1").innerHTML=arr.map(showentry).join("<br/>");
-		}
+var showdetails=function(tofind){
+	//var arr=entrySearch(tofind.replace(/\(\d+.\d+\)/,""));
+	document.getElementById("display2").innerHTML=drawtable(tofind);
+}
 
-	var showdetails=function(tofind){
-			//var arr=entrySearch(tofind.replace(/\(\d+.\d+\)/,""));
-			document.getElementById("display2").innerHTML=drawtable(tofind);
-		}
-
-	var drawtable = function(obj){
-			//	var columnNumber = 10;
-			return "<table border='1'><tbody><tr><td>"+obj.entry+"</td><td>藏文解釋 :"+obj.tdefinition+"</td><td>中文解釋 :"+obj.cdefinition+"</td></tr></tbody></table>";
-		}
+var drawtable = function(obj){
+	//	var columnNumber = 10;
+	return "<table border='1'><tbody><tr><td>"+obj.entry+"</td><td>藏文解釋 :"+obj.tdefinition+"</td><td>中文解釋 :"+obj.cdefinition+"</td></tr></tbody></table>";
+}
 		
